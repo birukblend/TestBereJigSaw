f8bf48416d8e73f2f02f3d7037452e7e706b1915
Biruk Yohannes <biruk.blend@gmail.com>
2024-10-10 18:28:46 +0300

    Updates

---
+++ precache-manifest.750be1e7089b7d1cf17e9ca8e8e4e62d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba90c51d18b430f6e12568d2c8b58fc1",
+    "url": "/TestBereJigSaw/index.html"
+  },
+  {
+    "revision": "5da5938d4d5e0ae3de40",
+    "url": "/TestBereJigSaw/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "32fbebc8ebd6bd2cc170",
+    "url": "/TestBereJigSaw/static/js/2.45abbce4.chunk.js"
+  },
+  {
+    "revision": "4dd206dec5ff825e678a",
+    "url": "/TestBereJigSaw/static/js/3.2ebb6cf0.chunk.js"
+  },
+  {
+    "revision": "5da5938d4d5e0ae3de40",
+    "url": "/TestBereJigSaw/static/js/main.ff143317.chunk.js"
+  },
+  {
+    "revision": "9ca1b8c726bb4916f41b",
+    "url": "/TestBereJigSaw/static/js/runtime~main.a1d9452c.js"
+  }
+]);
